9e992f7338b9a4737128ea246816f5a7c64b283d
LuisChaidezt2 <0321101254@ut-tijuana.edumx>
2024-10-29 01:45:46 -0700

    comments

--- app/src/main/java/com/example/examen2auth/SignInActivity.kt
+++ app/src/main/java/com/example/examen2auth/SignInActivity.kt
@@ -7,41 +7,53 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.examen2auth.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
+
 class SignInActivity : AppCompatActivity() {
 
+    // Variables para el enlace de vista (View Binding) y la autenticación de Firebase
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // layout usando View Binding
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Inicializar FirebaseAuth
         firebaseAuth = FirebaseAuth.getInstance()
+
+        // Configurar listener para el TextView que lleva a la pantalla de registro
         binding.textView.setOnClickListener {
+            // Redirige a SignUpActivity
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
+        // Configurar listener para el botón de inicio de sesión
         binding.button.setOnClickListener {
+            // Obtener el email y la contraseña de los campos de texto
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
 
+            // Verificar que los campos de email y contraseña no estén vacíos
             if (email.isNotEmpty() && pass.isNotEmpty()) {
 
+                //  iniciar sesión con Firebase usando email y contraseña
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
+                        // Si el inicio de sesión es exitoso, redirige a MainActivity
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                     } else {
+                        // Muestra un mensaje de error si el inicio de sesión falla
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
                     }
                 }
             } else {
+                // Muestra un mensaje si algún campo está vacío
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
             }
         }
     }
@@ -49,7 +61,9 @@ class SignInActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        if(firebaseAuth.currentUser != null){
+        // Verifica si el usuario ya está autenticado al iniciar la actividad
+        if (firebaseAuth.currentUser != null) {
+            // Si el usuario ya ha iniciado sesión, redirige a MainActivity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/examen2auth/SignUpActivity.kt
+++ app/src/main/java/com/example/examen2auth/SignUpActivity.kt
@@ -7,46 +7,60 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.examen2auth.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 
+
 class SignUpActivity : AppCompatActivity() {
 
+    // Variables para el enlace de vista (View Binding) y la autenticación de Firebase
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // layout usando View Binding
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Inicializar FirebaseAuth
         firebaseAuth = FirebaseAuth.getInstance()
 
+        // Configurar listener para el TextView que lleva a la pantalla de inicio de sesión
         binding.textView.setOnClickListener {
+            // Redirige a SignInActivity
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+
+        // Configurar listener para el botón de registro
         binding.button.setOnClickListener {
+            // Obtener el email, la contraseña y la confirmación de contraseña de los campos de texto
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
 
+            // Verificar que los campos no estén vacíos
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                // Comprobar que la contraseña y la confirmación coinciden
                 if (pass == confirmPass) {
 
+                    // crear el usuario en Firebase Authentication con email y contraseña
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if (it.isSuccessful) {
+                            // Si el registro es exitoso, redirige a SignInActivity
                             val intent = Intent(this, SignInActivity::class.java)
                             startActivity(intent)
                         } else {
+                            // Muestra un mensaje de error si el registro falla
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
                         }
                     }
                 } else {
+                    // Muestra un mensaje si las contraseñas no coinciden
                     Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
                 }
             } else {
+                // Muestra un mensaje si algún campo está vacío
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
             }
         }
     }
