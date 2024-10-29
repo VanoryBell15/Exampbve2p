02469734a63477a9ede3cdd580f52315302d0813
LuisChaidezt2 <0321101254@ut-tijuana.edumx>
2024-10-29 01:24:33 -0700

    first push on AS

---
+++ app/src/main/java/com/example/examen2auth/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.examen2auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.examen2auth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Usa el binding para el layout activity_main.xml
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Inicializa FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Configura el botón de logout
+        binding.logoutButton.setOnClickListener {
+            firebaseAuth.signOut()
+            // Redirige a SignInActivity
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+}
